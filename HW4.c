354553de7ea1858493977a96990218081f7c6c46
E1B55 <u11202255@o365.ttu.edu.tw>
2024-06-02 11:52:54 +0800

    第三步驟，利用for迴圈寫出，中途發現.c檔，問同學才知道int i要拉出來不能在裡面宣告

--- HW4.c
+++ HW4.c
@@ -3,7 +3,9 @@
 
 int main()
 {
-	
+	int count1 = 0, count2 = 0, count3 = 0, count4 = 0, count5 = 0;//宣告count1=0,count2=0,count3=0,count4=0,count5=0
+	char option;//宣告選項 
+
 	printf("                    .^^:                                    \n");
 	printf("                    !@@B^                                   \n");
 	printf("                    !@@@B   ~PPP!     :~~:                  \n");
@@ -46,7 +48,7 @@ int main()
         return 0; 
     }
     
-    while(true)
+    while(count5==0)
     {
     	 system("CLS");  // 清除螢幕
     //步驟2 
@@ -56,5 +58,83 @@ int main()
     printf("|  c. Choose by yourself |\n");
     printf("|  d. Exit               |\n");
     printf("--------------------------\n");
+    //步驟3
+	printf("請輸入字元:");
+	fflush(stdin);
+	option=getche();//打出一個字元，會顯示在螢幕上 	
+	switch(option)
+	{
+		case 'A'://顯示A的選單 
+		case 'a'://顯示a的選單 
+		count1=0;
+		while(count5==0)
+		{
+			 while (count1 == 0) 
+			{
+                    int n;
+                   
+                    printf("\n請輸入5到10的整數: "); // 輸入5到10的整數
+                    scanf("%d", &n);
+
+                    
+                    if (n < 5 || n > 10) {// n是否在5到10之間
+                        printf("錯誤: 請輸入5到10的整數。\n");
+                        continue; // 回到輸入n的步驟
+                    }
+
+                    
+                    char names[n][50];// 宣告儲存名字的陣列
+                    int num[n];// 宣告儲存學號的陣列
+                    int math[n], physics[n], english[n];// 宣告儲存成績的陣列
+
+                    int i;
+                    for (i = 0; i < n; i++)
+					{
+                        printf("輸入第%d個學生的姓名: ", i+1);
+                        scanf("%s", names[i]); // 輸入學生姓名
+
+                        printf("輸入第%d個學生的學號: ",i+1);
+                        scanf("%d", &num[i]); // 輸入學生學號
+                        if (num[i] < 100000 || num[i] > 999999)
+						{
+                            printf("錯誤!請修正。\n");
+                            i--; // 學號錯誤，返回重新輸入
+                            continue;
+                        }
+
+                        printf("輸入第%d學生的數學成績: ",i+1);
+                        scanf("%d",&math[i]); // 輸入數學成績
+                        if (math[i]<0||math[i]>100) 
+						{
+                            printf("錯誤!成績在0到100之間。\n");
+                            i--; // 若成績錯誤，返回重新輸入該學生資料
+                            continue;
+                        }
+
+                        printf("輸入第%d學生的物理成績: ",i+1);
+                        scanf("%d",&physics[i]); // 輸入物理成績
+                        if (physics[i]<0||physics[i]>100) 
+						{
+                            printf("錯誤!成績在0到100之間。\n");
+                            i--; // 若成績錯誤，返回重新輸入該學生資料
+                            continue;
+                        }
+
+                        printf("輸入第%d個學生的英文成績: ",i+1);
+                        scanf("%d",&english[i]); // 輸入英文成績
+                        if (english[i]<0||english[i]>100) 
+						{
+                            printf("錯誤!成績在0到100之間。\n");
+                            i--; // 若成績錯誤，返回重新輸入該學生資料
+                            continue;
+    					  }
+                    }
+
+                    count1 = 1; // 完成輸入，跳出內部while循環
+            }
+            break ;
+                
+ 		}
+ 	}
 	}
- } 
+}
